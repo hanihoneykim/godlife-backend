d723489da24305ee5c999e23b5f3bc182e0c559a
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2024-01-02 18:23:01 +0900

    add: Team model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
+import uuid
 from django.db import models
+from config.utils import compress_image, upload_path
+from common.models import CommonModel
+from user.models import User
 
-# Create your models here.
+
+class Team(CommonModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, null=False, blank=False)
+    name = models.CharField(max_length=30, null=False, blank=False)
+    description = models.CharField(max_length=300, null=False, blank=False)
+    image = models.ImageField(null=True, blank=True, upload_to=upload_path)
+    leader = models.ForeignKey(
+        User, null=True, related_name="team", on_delete=models.CASCADE
+    )
