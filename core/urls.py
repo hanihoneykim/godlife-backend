8b87de6f416dfb56b197988c6b6710af75322466
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2024-01-03 19:20:13 +0900

    add: MemberRemove

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TeamListCreate, TeamDetail, MemberListCreate
+from .views import TeamListCreate, TeamDetail, MemberListCreate, MemberRemove
 
 urlpatterns = [
     path("teams", TeamListCreate.as_view()),
     path("teams/<str:pk>", TeamDetail.as_view()),
     path("teams/<str:pk>/members", MemberListCreate.as_view()),
+    path("teams/<str:pk>/members/<str:user_pk>", MemberRemove.as_view()),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework import generics, status
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -68,3 +69,20 @@ class MemberListCreate(generics.ListCreateAPIView):
         team_pk = self.kwargs.get("pk", None)
         team_instance = get_object_or_404(Team, pk=team_pk)
         serializer.save(user=self.request.user, team=team_instance)
+
+
+class MemberRemove(generics.DestroyAPIView):
+    serializer_class = MemberSerializer
+    permission_classes = (IsLeaderPermission,)
+    queryset = Member.objects.all()
+
+    def delete(self, request, *args, **kwargs):
+        pk = self.kwargs.get("pk")
+        user_pk = self.kwargs.get("user_pk")
+
+        member = Member.objects.filter(team=pk, user=user_pk).first()
+        if member:
+            member.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
